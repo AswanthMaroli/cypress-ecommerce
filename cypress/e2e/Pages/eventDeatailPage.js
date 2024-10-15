db1740159d27fa7930fa73e302ea9a16ef5ffe77
AswanthMaroli <aswanthaswu07@gmail.com>
2024-10-15 16:34:16 +0530

    event details tests

--- cypress/e2e/Pages/eventDeatailPage.js
+++ cypress/e2e/Pages/eventDeatailPage.js
@@ -1,35 +1,137 @@
 class EventDetails {
 
-    homeurl = 'https://test.eventzet.com/#/Eventshell/Eventhome';
-    loginMenu = ':nth-child(2) > :nth-child(8) > #EventZetHomemenuLoginmenu';
+    loginMenuButton = ':nth-child(2) > :nth-child(8) > #EventZetHomemenuLoginmenu';
     userEmail = '#UserEmail';
     userPassword = '#UserPassword';
-    loginButton = '#EventLoginBtn';
+    login = '#EventLoginBtn';
     searchEventButton = '#SearchEventHome';
     allEvents = '#targetFilterEvent';
     eventName = '.h4';
     registerNowButton = '#EventDetailsRegisterNowBtn'
+    getTicketsButton = '#EventDetailsRegisterNowBtn';
+    eventVenue = ':nth-child(2) > .col-md-12 > .btn > .justify-content-between';
+    eventTitle =  '.section-title > .clr_base';
+    onlineTitle = ':nth-child(8) > .col-md-12 > .btn > .justify-content-between';
+    timeSlot ='.btn > .justify-content-between';
+    continueButton ='#EventDetailsRegistrBtn';
+    closeIcon ='.d-flex > :nth-child(3) > .bi';
+    backIcon = '.d-flex > :nth-child(1) > .bi';
 
+    clickCloseIcon(){
 
-
-    checkRegistrationPopup() {
-
-        cy.get('.modal-body').should('be.visible');
+        cy.get(this.closeIcon).should('be.visible').click({force:true});
 
     }
 
-    checkUserName(username) {
+    clickBackIcon(){
 
-        cy.get('.text-secondary > .text-dark').should('contain', username);
+        cy.get(this.backIcon).should('be.visible').click({force:true});
 
     }
 
-    checkTicketDetails(ticketdetails) {
 
-        cy.get('.col-md-5 > .card > .card-body > .row > :nth-child(3)')
-            .should('be.visible')
-            .should('contain', ticketdetails);
+    clickContinueButton(){
+
+        cy.get(this.continueButton).should('be.visible').click({force:true});
+        cy.wait(1000);
+        cy.url().should('include','https://test.eventzet.com/#/eventregistration/Eventshell/Eventregistration');
+
     }
+
+    clickGetTicketsButton(){
+
+        cy.get(this.getTicketsButton).scrollIntoView().should('be.visible').click();
+    }
+
+    selectVenue(){
+
+        cy.get(this.eventVenue).should('be.visible').click({force:true});
+    }
+
+    selectTimeSlot(){
+
+        cy.get(this.timeSlot).should('be.visible').click({force:true});
+    }
+
+    checkEventTickets(ticketname1,ticketname2,addonname1,addonname2){
+
+        cy.get(':nth-child(2) > .d-flex.bd-highlight > .fw-bold').should('contain',ticketname1);
+        cy.get(':nth-child(3) > .d-flex.bd-highlight > .fw-bold').should('contain',ticketname2);
+        cy.get(':nth-child(2) > .bg_lightash').should('contain',addonname1);
+        cy.get(':nth-child(3) > .bg_lightash').should('contain',addonname2);
+    }
+
+    selectFreeTicket(quantity){
+
+        cy.get(':nth-child(2) > .d-flex.bd-highlight > :nth-child(4) > .input-group > #ticket_number')
+        .should('be.visible')
+        .clear()
+        .type(quantity);
+
+    }
+
+    selectFreeTicketAddon(quantity){
+
+        cy.get(':nth-child(2) > .bg_lightash > :nth-child(3) > .input-group > #addon_number').should('be.visible')
+        .clear()
+        .type(quantity);
+
+    }
+
+    selectPaidTicket(quantity){
+
+        cy.get(':nth-child(3) > .d-flex.bd-highlight > :nth-child(4) > .input-group > #ticket_number')
+        .should('be.visible')
+        .clear().type(quantity);
+
+    }
+
+    selectPaidTicketAddon(quantity){
+
+        cy.get(':nth-child(3) > .bg_lightash > :nth-child(3) > .input-group > #addon_number')
+        .should('be.visible')
+        .clear().type(quantity);
+
+    }
+
+    checkEventTimeSlot(date,time,timezone){
+
+        cy.get(this.timeSlot).should('be.visible').should('contain',date);
+        cy.get(this.timeSlot).should('contain',time);
+        cy.get(this.timeSlot).should('contain',timezone);
+
+
+    }
+
+
+    checkEventVenue(eventtitle,eventlocation,onlinetitle){
+
+        cy.get(this.eventTitle).should('be.visible').should('contain',eventtitle);
+        cy.get(this.eventVenue).should('be.visible').should('contain',eventlocation);
+        cy.get(this.onlineTitle).should('be.visible').should('contain',onlinetitle);
+        
+    }
+
+
+    checkEventDetailsDatas(eventname,organizername,organizernumber,organizeremail,specialinstructions,eventlocation,onlineticket,ticketname1,addonname1,ticketname2,addonname2){
+
+        cy.get('.h4').should('contain',eventname);
+        cy.get('.text-secondary > .text-dark').should('contain',organizername);
+        cy.get(':nth-child(1) > :nth-child(2) > .d-flex > :nth-child(2)').should('contain',organizernumber);
+        cy.get('.d-flex > :nth-child(3)').should('contain',organizeremail);
+        cy.get('.text-dark > span').should('contain',specialinstructions);
+        cy.get(':nth-child(4) > :nth-child(2) > .list-inline > :nth-child(1) > div').should('contain',eventlocation);
+        cy.get('.row > :nth-child(3) > :nth-child(2) > .text-dark').should('contain',ticketname1);
+        cy.get(':nth-child(4) > .list-inline > .list-inline-item.font_p').should('contain',addonname1);
+        cy.get(':nth-child(3) > .text-dark').should('contain',onlineticket);
+        cy.get(':nth-child(4) > .text-dark').should('contain',ticketname2);
+        cy.get(':nth-child(5) > .list-inline > .list-inline-item.font_p').should('contain',addonname2);
+
+    }
+
+
+
+
 
     clickCopyUrl() {
 
@@ -37,34 +139,15 @@ class EventDetails {
 
     }
 
-    checkEventLocation(eventlocation) {
-
-        cy.get('.col-md-12.my-2 > .card > .card-body').should('contain', eventlocation);
-
-    }
-
-    checkRegisterNowButton() {
-
-        cy.get(this.registerNowButton).should('be.visible');
-
-    }
-
-    clickRegisterNowButton() {
-
-        cy.get(this.registerNowButton).click({ force: true });
-
-    }
-
-    checkEventName(eventname) {
-
-        cy.get(this.eventName).should('contain', eventname);
-    }
 
     clickEvent(eventname) {
 
-        cy.get('#targetFilterEvent')
-            .contains(eventname)
-            .click({ force: true });
+        cy.get('#Eventssection > :nth-child(1)').scrollIntoView();
+        cy.wait(2000);
+        cy.get('#Eventssection > :nth-child(1)')
+        .should('be.visible')
+        .contains(eventname)
+        .click();
 
     }
 
@@ -76,41 +159,28 @@ class EventDetails {
 
     inputEmail(useremail) {
 
-        cy.get(this.userEmail).click({ force: true }).clear().type(useremail);
+        cy.get(this.userEmail).click({ force: true }).type(useremail);
 
     }
 
-    inputPassword(password) {
+    inputPassword(userepassword) {
 
-        cy.get(this.userPassword).click({ force: true }).clear().type(password);
+        cy.get(this.userPassword).click({ force: true }).type(userepassword);
+
+    }
+
+    clickLoginMenu() {
+
+        cy.get(this.loginMenuButton).click();
 
     }
 
     loginClick() {
 
-        cy.get(this.loginButton).click({ force: true });
+        cy.get(this.login).click();
+        cy.wait(6000);
+
     }
-
-    clickLoginMenu() {
-
-        cy.get(this.loginMenu).click({ force: true });
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- cypress/e2e/Tests/eventDetails.cy.js
+++ cypress/e2e/Tests/eventDetails.cy.js
@@ -1,196 +1,138 @@
 import EventDetails from '../../e2e/Pages/eventDeatailPage.js';
 const { readDataFromFile } = require('../ExternalFiles/fileOperations.js');
 const baseUrl = Cypress.config('baseUrl');
-const filename = 'cypress/fixtures/createEventBasicInfoRead.json';
-const filename1 = 'cypress/fixtures/createEventAddTicketRead.json';
 
-const ed = new EventDetails();
+const filename = 'cypress/fixtures/createEventBasicInfoRead.json';
+
+const eventDetails = new EventDetails();
+
+function performLogin(email, password) {
+    cy.visit(baseUrl);
+    cy.wait(4000);
+    eventDetails.clickLoginMenu();
+    eventDetails.inputEmail(email);
+    eventDetails.inputPassword(password);
+    eventDetails.loginClick();
+}
+
+
+
 
 module.exports = {
 
 
     EventDetailsTests: [
-        //test 1
-
-
 
         it("Test 1:Check event details page is visible or not", () => {
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
+
             readDataFromFile(filename).then((list) => {
 
-                ed.clickEvent(list.eventtitle);
+                performLogin(list.email, list.password);
 
+                eventDetails.clickEvent(list.eventtitle);
+                cy.intercept('GET', '/api/EventDetails/GetEventVideoURL?EventID=*').as('eventDetails');
+                cy.wait('@eventDetails', { timeout: 25000 });
+                cy.url().should('include', 'https://test.eventzet.com/#/eventregistration/Eventshell/Eventdetails');
             });
-            cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
-            cy.wait('@eventDetails', { timeout: 25000 });
-
-
-            cy.url().should('include', 'https://test.eventzet.com/#/eventregistration/Eventshell/Eventdetails');
-
             cy.log('Event details page is visible');
         }),
-        //test 2
 
-        it("Test 2:Check event name is present in the event details page", () => {
-
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
+        it("Test 2:Check details on the eventdetails page", () => {
             readDataFromFile(filename).then((list) => {
 
-                ed.clickEvent(list.eventtitle);
-
-                cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
+                performLogin(list.email, list.password);
+              
+                eventDetails.clickEvent(list.eventtitle);
+                cy.intercept('GET', '/api/EventDetails/GetEventVideoURL?EventID=*').as('eventDetails');
                 cy.wait('@eventDetails', { timeout: 25000 });
-                ed.checkEventName(list.eventtitle);
+                cy.url().should('include', 'https://test.eventzet.com/#/eventregistration/Eventshell/Eventdetails');
+                eventDetails.checkEventDetailsDatas(
+
+                    list.eventtitle,
+                    list.eventcreator,
+                    list.organizerphone,
+                    list.organizeremail,
+                    list.instructions1,
+                    list.eventlocation1,
+                    list.onlineticket,
+                    list.ticketname1,
+                    list.addonname1,
+                    list.ticketname2,
+                    list.addonname2
+
+                );
+
+                eventDetails.clickCopyUrl();
                 cy.wait(2000);
 
             });
-
-            cy.log('Event name is present in the event details page');
+            cy.log('All the details where present on the eventdetails page ');
         }),
 
-        //test 3
 
         it("Test 3:Check event ticket details is present in the event details page", () => {
 
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
+           
             readDataFromFile(filename).then((list) => {
 
-                ed.clickEvent(list.eventtitle);
+                performLogin(list.email, list.password);
+                eventDetails.clickEvent(list.eventtitle);
+                cy.intercept('GET', '/api/EventDetails/GetEventVideoURL?EventID=*').as('eventDetails');
+                cy.wait('@eventDetails', { timeout: 25000 });
+                cy.url().should('include', 'https://test.eventzet.com/#/eventregistration/Eventshell/Eventdetails');
+                eventDetails.clickGetTicketsButton();
+                cy.wait(1000);
+                eventDetails.checkEventVenue(
+                
+                    list.eventtitle,
+                    list.eventlocation1,
+                    list.onlinetitle
+                );
+                eventDetails.selectVenue();
+                eventDetails.checkEventTimeSlot(
+                    list.timeslotdate,
+                    list.timeslottime,
+                    list.timezone
+                );
 
-
-            });
-
-            cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
-            cy.wait('@eventDetails', { timeout: 25000 });
-
-            readDataFromFile(filename1).then((list) => {
-
-                ed.checkTicketDetails(list.tiername);
-                ed.checkTicketDetails(list.tiername2);
-                ed.checkTicketDetails(list.addonName1);
-                ed.checkTicketDetails(list.addonName2);
-                cy.wait(2000);
-
+                eventDetails.selectTimeSlot();
+                eventDetails.selectFreeTicket(list.ticketquantity);
+                eventDetails.selectPaidTicket(list.ticketquantity);
+                eventDetails.checkEventTickets(
+                    
+                    list.ticketname1,
+                    list.ticketname2,
+                    list.addonname1,
+                    list.addonname2
+                );
             });
 
             cy.log('Event ticket details is present in the event details page');
         }),
-        //test 4
 
-        it("Test 4:Check username is present in the event details page", () => {
 
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
+        it("Test 4:Verify that user can select ticket and continue to the registration page ", () => {
+
             readDataFromFile(filename).then((list) => {
 
-                ed.clickEvent(list.eventtitle);
-
-
-            });
-            cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
-            cy.wait('@eventDetails', { timeout: 25000 });
-            ed.checkUserName(' jr James ');
-            cy.wait(2000);
-
-            cy.log('Username is present in the event details page');
-        }),
-        //test 5
-
-
-        it("Test 5:Check event location is present in the event details page", () => {
-
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
-            readDataFromFile(filename).then((list) => {
-
-                ed.clickEvent(list.eventtitle);
-                cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
+                performLogin(list.email, list.password);
+                eventDetails.clickEvent(list.eventtitle);
+                cy.intercept('GET', '/api/EventDetails/GetEventVideoURL?EventID=*').as('eventDetails');
                 cy.wait('@eventDetails', { timeout: 25000 });
-
-                ed.checkEventLocation(list.eventlocation);
-                cy.wait(2000);
-
+                cy.url().should('include', 'https://test.eventzet.com/#/eventregistration/Eventshell/Eventdetails');
+                eventDetails.clickGetTicketsButton();
+                cy.wait(1000);
+                eventDetails.selectVenue();
+                eventDetails.selectTimeSlot();
+                eventDetails.selectFreeTicket(list.ticketquantity);
+                eventDetails.selectFreeTicketAddon(list.addonquantity);
+                eventDetails.selectPaidTicket(list.ticketquantity);
+                eventDetails.selectPaidTicketAddon(list.addonquantity);
+                eventDetails.clickContinueButton();
             });
 
-
-            cy.log('Event location is present in the event details page');
+            cy.log('Test 4 is successful and user is navigated to the contact information page');
         }),
-        //test 6
-
-
-        it("Test 6:Check RegisterNow button is present in the event details page", () => {
-
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
-            readDataFromFile(filename).then((list) => {
-
-                ed.clickEvent(list.eventtitle);
-
-            });
-            cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
-            cy.wait('@eventDetails', { timeout: 25000 });
-            ed.checkRegisterNowButton();
-            cy.wait(2000);
-
-            cy.log('RegisterNow button is present in the event details page');
-        }),
-        //test 7
-
-        it("Test 7:Check Copy url button is clickable or not", () => {
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
-            readDataFromFile(filename).then((list) => {
-
-                ed.clickEvent(list.eventtitle);
-
-            });
-            cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
-            cy.wait('@eventDetails', { timeout: 25000 });
-            ed.clickCopyUrl();
-            cy.wait(2000);
-
-            cy.log('CopyUrl button is clickable');
-        }),
-        //test 8
-
-        it("Test 8:Check RegisterNow button is clickable or not", () => {
-            cy.visit(baseUrl);
-            cy.wait(2000);
-            ed.clickSearchEvent();
-            cy.wait(4000);
-            readDataFromFile(filename).then((list) => {
-
-                ed.clickEvent(list.eventtitle);
-
-            });
-            cy.intercept('GET', '/api/EventRegistration/GetEventAddonQuantity?EventID=*&EventRegID=*').as('eventDetails');
-            cy.wait('@eventDetails', { timeout: 25000 });
-            ed.clickRegisterNowButton();
-            cy.wait(2000);
-            ed.checkRegistrationPopup();
-
-            cy.log('RegisterNow button is clickable');
-        }),
-
-
-
 
 
     ]
